07586820904853d68863981020198463d085a7b3
HoangNguyetHa0310 <hoangyh2712@gmail.com>
2025-06-02 14:47:17 +0700

    Start project

---
+++ src/main/java/dev/phanhoang/storeweb_springvue/StoreWebSpringVueApplication.java
@@ -0,0 +1,13 @@
+package dev.phanhoang.storeweb_springvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWebSpringVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreWebSpringVueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/phanhoang/storeweb_springvue/StoreWebSpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phanhoang.storeweb_springvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWebSpringVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
